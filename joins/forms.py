11db54e50a69f11585d7d6b3be84e295f9f3db8f
chenlongjiu <stanleyclj@gmail.com>
2015-07-30 21:58:16 -0400

    form

--- joins/forms.py
+++ joins/forms.py
@@ -1,6 +1,12 @@
 # getting a form
 from django import forms
+from .models import Join
 
 class EmailForm(forms.Form):
 	name = forms.CharField(required = False)
 	email = forms.EmailField(required = True)
+
+
+class JoinForm(forms.ModelForm):
+	class Meta:
+		model = Join
--- joins/views.py
+++ joins/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 
-from .forms import EmailForm
+from .forms import EmailForm, JoinForm
 from .models import Join
 # Create your views here.
 
 def home(request):
 	#print request.POST
-	form = EmailForm(request.POST)
-	if form.is_valid():
+	#using django form method 
+	# form = EmailForm(request.POST)
+	# if form.is_valid():
+	# 	email =  form.cleaned_data['email']
+	# 	new_join , create = Join.objects.get_or_create(email = email)
+	# 	print new_join, create
+	# 	if create: 
+	# 		print Created
+
+	#this is using model forms
+	form = JoinForm(request.POST or None)
+	if form.is_valid:
+		#new_join = form.save(commit=False)
 		email =  form.cleaned_data['email']
-		new_join , create = Join.objects.get_or_create(email = email)
-		print new_join, create
-		if create: 
-			print "Created"
+		new_join2 , create = Join.objects.get_or_create(email = email)
+		#new_join.save() #ok with duplicate
 	context = {"form":form}
 	template = "home.html"
 	return render(request, template, context)
